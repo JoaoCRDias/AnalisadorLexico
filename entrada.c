e298b5a6a5731cbbd7fc3e32cd28e826f1246616
Kolisium <joaocrdias17@gmail.com>
2017-04-18 10:36:44 -0300

    Add files via upload

---
+++ entrada.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+char variavel;
+variavel = 10;
+if (variavel == 10) {
+  int variavel1;
+  if (variavel1 == 10) {
+    variavel1 = 87;
+    int cont;
+    cont = 0;
+    >>
+    8aa a = 1
+    while (cont < 2) {
+      ---
+      !
+      **
+      ^^
+      int a;
+      print cont;/*  coment
+      ate aqui  */
+      //outro coment
+      cont = a + 1;
+    }
+  } else {
+    int x;
+    x = 700;
+    if (x == 100.2) {
+      x = 700;
+    } else {
+      x = 9;
+    }
+  }
+}
+x = 0;
+float y;
+y = 1;
+print x;
+x = 19;
+x = 640;
+while (cont <= 2) {
+  printf "Analisador Sintatico\n";
+  cont = 1;
+}
